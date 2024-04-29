23c2b67da1c8597c35ed5aa7ef8f30fd107902d8
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-29 15:14:56 +0300

    Lesson 4

--- NewsProject/News/models.py
+++ NewsProject/News/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='media/%Y/%M/%D')
+    is_published = models.BooleanField(default=True)
