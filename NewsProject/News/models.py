d5dd0c53a93e7fe94900d41d023acf19a35eeded
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-30 18:17:35 +0600

    Lesson 6

--- NewsProject/News/models.py
+++ NewsProject/News/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='media/%Y/%M/%D')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name = 'Заголовок')
+    content = models.TextField(blank=True, verbose_name = 'Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name = 'Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name = 'Время изменения')
+    photo = models.ImageField(upload_to='media/%Y/%M/%D', verbose_name = 'Фото')
+    is_published = models.BooleanField(default=True, verbose_name = 'Публикация')
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
